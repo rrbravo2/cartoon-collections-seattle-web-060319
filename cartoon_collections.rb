674f64f88eef5317114b7de8e165d770158b8379
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 07:15:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(dwarves)# code an argument here
  end
 end 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)# code an argument here
+  veggies.collect do |call|# Your code here
+  call.capitalize + "!"
+ end
 end
 
 def long_planeteer_calls# code an argument here
