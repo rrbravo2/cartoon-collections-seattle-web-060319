02865f48fd8eae224413364b44414340825a977a
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 07:22:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(calls_long)# code an argument here
   array.any? do |words|# Your code here
    words.length > 4
  end
-
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
