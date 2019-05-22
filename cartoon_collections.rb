5a25ae44d269aff22cb934405bd2eb025af8d683
Ryan Bravo <rrbravo2@gmail.com>
2019-05-22 07:20:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(veggies)# code an argument here
  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)# code an argument here
+  array.any? do |words|# Your code here
+   words.length > 4
 end
 
 def find_the_cheese# code an argument here
